abcb40a446af716c8832d0a2eab407d9c946eae0
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2022-12-30 16:32:58 +0200

    Update Menus Templates and Views

--- baby_food/baby_food/menu_app/views.py
+++ baby_food/baby_food/menu_app/views.py
@@ -1,9 +1,10 @@
 import datetime
 from http.client import HTTPResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from baby_food.menu_app.models import MenuWithoutAllergens
+from baby_food.accounts.models import Profile
+from baby_food.menu_app.models import MenuWithoutAllergens, Menu
 
 
 def menu_home(request):
@@ -11,21 +12,16 @@ def menu_home(request):
 
 
 def menu_with_allergens_first(request):
+    user = request.user
+    # menus = MenuWithoutAllergens.objects.filter(date__gt=(datetime.date.today() + datetime.timedelta(days=10)))
+    menus = Menu.objects.filter(age__exact='10M-18M')
+    try:
+        profile = Profile.objects.get(user_id=user.id)
+    except Profile.DoesNotExist:
+        profile = None
 
     context = {}
 
-
-    return render(request, 'menus/menu-with-allergens-first.html', context)
-
-
-def menu_with_allergens_last(request):
-    return render(request, 'menus/menu-with-allergens-last.html')
-
-
-def menu_no_allergens(request):
-    # menus = MenuWithoutAllergens.objects.filter(date__gt=(datetime.date.today() + datetime.timedelta(days=2)))
-    menus = MenuWithoutAllergens.objects.all()
-    context = {}
     if menus.count() > 5:
         menus_first = menus[:5]
         menus_second = menus[5:10]
@@ -33,6 +29,52 @@ def menu_no_allergens(request):
         context['menus_second'] = menus_second
     else:
         context['menus_first'] = menus
+        context['profile'] = profile
 
+    return render(request, 'menus/menu-with-allergens-first.html', context)
+
+
+def menu_with_allergens_last(request):
+    user = request.user
+    # menus = Menu.objects.filter(age__exact='18M-36M', date__gt=(datetime.date.today() + datetime.timedelta(days=10)))
+    menus = Menu.objects.filter(age__exact='18M-36M')
+    try:
+        profile = Profile.objects.get(user_id=user.id)
+    except Profile.DoesNotExist:
+        profile = None
+
+    context = {}
+
+    if menus.count() > 5:
+        menus_first = menus[:5]
+        menus_second = menus[5:10]
+        context['menus_first'] = menus_first
+        context['menus_second'] = menus_second
+    else:
+        context['menus_first'] = menus
+        context['profile'] = profile
+
+    return render(request, 'menus/menu-with-allergens-last.html', context)
+
+
+def menu_no_allergens(request):
+    user = request.user
+    # menus = MenuWithoutAllergens.objects.filter(date__gt=(datetime.date.today() + datetime.timedelta(days=10)))
+    menus = MenuWithoutAllergens.objects.all()
+    try:
+        profile = Profile.objects.get(user_id=user.id)
+    except Profile.DoesNotExist:
+        profile = None
+
+    context = {}
+
+    if menus.count() > 5:
+        menus_first = menus[:5]
+        menus_second = menus[5:10]
+        context['menus_first'] = menus_first
+        context['menus_second'] = menus_second
+    else:
+        context['menus_first'] = menus
+        context['profile'] = profile
 
     return render(request, 'menus/menu-no-allergens.html', context)
